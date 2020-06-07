e7fde6cb6f42e93189a6318e8b1efd4c9303f257
YosephAhmed <yosephahmed@aol.com>
2020-06-07 14:03:37 -0400

    Added a UserProfile component that displays username and member-since. Added a link from Home component to UserProfile component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Components/Home';
+import UserProfile from './Components/UserProfile';
 
 class App extends Component {
     constructor() {
         super();
 
         this.state = {
-            accountBalance: 14568.27
+            accountBalance: 14568.27,
+            currentUser: {
+                userName: 'bob_loblaw',
+                memberSince: '08/23/99',
+            }
         }
     }
 
     render() {
 
         const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+        const UserProfileComponent = () => (<UserProfile userName = {this.state.currentUser.userName} memberSince = {this.state.currentUser.memberSince}/>);
 
         return (
             <Router>
-                <Switch>
+                <div>
                     <Route exact path="/" render={HomeComponent} />
-                </Switch>
+                    <Route exact path="/userProfile" render={UserProfileComponent} />
+                </div>
             </Router>
         );
     }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import AccountBalance from './AccountBalance';
+import {Link} from 'react-router-dom';
 
 class Home extends Component {
     render() {
@@ -7,6 +8,8 @@ class Home extends Component {
             <div>
                 <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank" />
                 <h1>Bank of React</h1>
+
+                <Link to="/userProfile">User Profile</Link>
 
                 <AccountBalance accountBalance={this.props.accountBalance}/>
             </div>
