51591d9de20ab72990d4f7281eb535e1dc372a58
YosephAhmed <yosephahmed@aol.com>
2020-06-07 14:05:50 -0400

    Added a link in UserProfile component that directs user to Home component

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,16 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class UserProfile extends Component {
-  render() {
-    return (
-        <div>
-          <h1>User Profile</h1>
+    render() {
+        return (
+            <div>
+                <Link to="/">Home</Link>
+                <h1>User Profile</h1>
 
-          <div>Username: {this.props.userName}</div>
-          <div>Member Since: {this.props.memberSince}</div>
-        </div>
-    );
-  }
+                <div>Username: {this.props.userName}</div>
+                <div>Member Since: {this.props.memberSince}</div>
+            </div>
+        );
+    }
 }
 
 export default UserProfile;
