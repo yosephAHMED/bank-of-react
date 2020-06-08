817989ef46c2bd4d0ec1d581dfe6928845e5497b
YosephAhmed <yosephahmed@aol.com>
2020-06-07 21:22:44 -0400

    Added LogIn component and gave it a Redirect.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import UserProfile from './Components/UserProfile';
+import LogIn from './Components/LogIn';
 
 class App extends Component {
     constructor() {
@@ -16,9 +17,16 @@ class App extends Component {
         }
     }
 
+    mockLogIn = (logInInfo) => {
+        const newUser = {...this.state.currentUser}
+        newUser.userName = logInInfo.userName;
+        this.setState({currentUser: newUser});
+    }
+
     render() {
 
         const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+        const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} {...this.props}/>);
         const UserProfileComponent = () => (<UserProfile userName = {this.state.currentUser.userName} memberSince = {this.state.currentUser.memberSince}/>);
 
         return (
@@ -26,6 +34,7 @@ class App extends Component {
                 <div>
                     <Route exact path="/" render={HomeComponent} />
                     <Route exact path="/userProfile" render={UserProfileComponent} />
+                    <Route exact path="/login" render={LogInComponent}/>
                 </div>
             </Router>
         );
